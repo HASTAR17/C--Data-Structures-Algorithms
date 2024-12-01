f366f7ccfb49b2b8cf25269b467091929279c979
HASTAR17 <abdullahalarafat147@gmail.com>
2024-12-01 15:07:14 +0600

    Initial Commit2

--- practice.cpp
+++ practice.cpp
@@ -12,13 +12,55 @@ int main()
     // float c = 4.0f;  // 4 byte
     // double d = 8.00; // 8 byte
     // bool e = true;  // 1 byte
-    
+
 
     // cout<<"int a = "<<a<<"\nchar b = "<<b<<"\nfloat c = "<<c<<"\ndouble d = "<<d<<"\nbool e = "<<e<<endl;
-    char grade = 'A';
+   
+   
+//Type Casting
+//     char grade = 'A';
 
-    int Grade = grade; // implicit coversion(char to int), that done automatically by compiler
-    cout << Grade;
+//     int Grade = grade; // implicit coversion(char to int), that done automatically by compiler
+//     cout << Grade<<endl;
+
+//     double mark=100.00;
+//     int finalMark = (int)mark;//Explicit type casting that we need to do
+//     cout<<finalMark<<endl;
+
+//Input in C++
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // cout << "Your age is: " << age << endl;
+
+//Operators
+
+    double a = 10;
+    int b = 3;
+
+    // cout << "Addition: " << a + b << endl;
+    // cout << "Subtraction: " << a - b << endl;
+    // cout << "Multiplication: " << a * b << endl;
+    // cout << "Division: " << a / b << endl;
+    // cout << "Modulus: " << a % b << endl;
+
+    // // Increment and Decrement Operators
+    // int x = 10;
+    // cout << "x++: " << x++ << endl; // post increment
+    // cout << "x: " << x << endl;
+
+    // x = 10;
+
+//boolean Operators
+    // cout << "++x: " << ++x << endl; // pre increment
+    // cout << (3<5)<<endl; //true->1
+    // cout << (3>5)<<endl; //false->0
+    // cout << (3<=3)<<endl; //true->1
+    // cout << (3!=5)<<endl; //true->1
+
+//Logical operators OR, AND,
+
 
     return 0;
 }
